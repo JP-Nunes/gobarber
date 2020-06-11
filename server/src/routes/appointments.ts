e31d7323f651387ff0d3180b456f315694e9ae4e
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-06-11 15:02:00 -0300

    Using routes, models and repositories to work with DTOs

---
+++ server/src/routes/appointments.ts
@@ -0,0 +1,31 @@
+import { Router } from 'express'
+import { startOfHour, parseISO } from 'date-fns'
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository'
+
+const appointmentsRouter = Router()
+const appointmentsRepository = new AppointmentsRepository()
+
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentsRepository.all()
+
+  return response.json(appointments)
+})
+
+appointmentsRouter.post('/', (request, response) => {
+  const { provider, date } = request.body
+
+  const parsedDate = startOfHour(parseISO(date))
+
+  const findSameDateAppointments = appointmentsRepository.findByDate(parsedDate)
+
+  if(findSameDateAppointments) {
+    return response.status(400).json({ message: 'Horário indisponível' })
+  }
+
+  const appointment = appointmentsRepository.create({ provider, date: parsedDate })
+
+  return response.json({ appointment })
+})
+
+export default appointmentsRouter
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express'
+import appointmentsRouter from './appointments'
 
 const routes = Router()
 
+routes.use('/appointments', appointmentsRouter)
+
 routes.get('/', (request, response) => {
-  return response.json({ message: "I'm sorry" })
+  return response.json({ message: "Hello moto" })
 })
 
 export default routes
