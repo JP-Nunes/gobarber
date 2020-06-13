516102e6e9f5378ea2481b891911261b86ec1d0e
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-06-13 17:10:56 -0300

    Adding generation of uuid by default

--- server/src/database/migrations/1591929694097-CreateAppointments.ts
+++ server/src/database/migrations/1591929694097-CreateAppointments.ts
@@ -11,7 +11,8 @@ export class CreateAppointments1591929694097 implements MigrationInterface {
             name: 'id',
             type: 'varchar',
             isPrimary: true,
-            generationStrategy: 'uuid'
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()'
           },
           {
             name: 'provider',
