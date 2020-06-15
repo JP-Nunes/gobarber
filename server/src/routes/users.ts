c9c3983631ee29335d5d3ca30521e00a7a296d11
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-06-15 00:27:01 -0300

    Using multer to receive and save image uploads

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express'
+import multer from 'multer'
+import uploadConfig from '../config/upload'
 
 import CreateUserService from '../services/CreateUserService'
 
+import ensureAuthentication from '../middlewares/ensureAuthentication'
+
 const usersRouter = Router()
+const upload = multer(uploadConfig)
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -22,5 +27,14 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: error.message })
   }
 })
+
+usersRouter.patch(
+    '/avatar',
+    ensureAuthentication,
+    upload.single('avatar'),
+    async (request, response) => {
+    return response.json({ ok: true })
+  }
+)
 
 export default usersRouter
