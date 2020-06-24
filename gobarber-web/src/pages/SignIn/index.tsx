e25dd26dcc723ce0432729bdee7bbaa130d76d9a
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-06-24 12:03:55 -0300

    Building sign in page

--- gobarber-web/src/pages/SignIn/index.tsx
+++ gobarber-web/src/pages/SignIn/index.tsx
@@ -1,5 +1,35 @@
 import React from 'react'
+import { FiLogIn } from 'react-icons/fi'
 
-const SignIn: React.FC = () => <h1>SignIn</h1>
+import logoImg from '../../assets/logo.svg'
+
+import { Container, Content, Background } from './styles'
+
+const SignIn: React.FC = () => (
+  <Container>
+    <Content>
+      <img src={logoImg} alt="GoBarber logo"/>
+
+      <form>
+        <h1>Faça seu logon</h1>
+
+        <input placeholder="Email"/>
+
+        <input type="password" placeholder="Senha"/>
+
+        <button type="submit">Entrar</button>
+
+        <a href="#">Esqueci minha senha</a>
+      </form>
+
+      <a href="#">
+        <FiLogIn />
+        Criar Conta
+      </a>
+    </Content>
+
+    <Background />
+  </Container>
+)
 
 export default SignIn
