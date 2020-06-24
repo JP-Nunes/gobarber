b6fffc0b76ae672e7e3615194ebe2967ebc83279
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-06-24 00:11:04 -0300

    Initiang application's frontend web

---
+++ gobarber-web/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import GlobalStyle from './styles/global'
+import SignIn from './pages/SignIn'
+
+function App () {
+  return (
+    <>
+      <SignIn />
+      <GlobalStyle />
+    </>
+  )
+}
+
+export default App
