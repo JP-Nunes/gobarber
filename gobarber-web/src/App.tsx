5174e9e70cb1a167174057f74cf79939569e3735
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-06-26 12:14:33 -0300

    Creating ToastContainer component

--- gobarber-web/src/App.tsx
+++ gobarber-web/src/App.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 import GlobalStyle from './styles/global'
 import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
+// import SignUp from './pages/SignUp'
 
+import ToastContainer from './components/ToastContainer'
 import { AuthProvider } from './hooks/AuthContext'
 
 const App: React.FC = () => {
@@ -12,6 +13,8 @@ const App: React.FC = () => {
       <AuthProvider>
         <SignIn />
       </AuthProvider>
+
+      <ToastContainer />
 
       <GlobalStyle />
     </>
