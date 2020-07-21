a5f84f4992b7025bb94ef4d223302b42b31a2ac3
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-07-20 23:03:53 -0300

    Improving usability

--- gobarber-mobile/src/components/Input/index.tsx
+++ gobarber-mobile/src/components/Input/index.tsx
@@ -1,4 +1,9 @@
-import React, { useEffect, useRef } from 'react'
+import React, {
+  useEffect,
+  useRef,
+  useImperativeHandle,
+  forwardRef,
+} from 'react'
 import { TextInputProps } from 'react-native'
 import { useField } from '@unform/core'
 
@@ -13,11 +18,24 @@ interface InputValueReference {
   value: string
 }
 
-const Input: React.FC<InputProps> = ({ name, icon, ...remainder }) => {
+interface InputRef {
+  focus(): void
+}
+
+const Input: React.RefForwardingComponent<InputRef, InputProps> = (
+  { name, icon, ...remainder },
+  ref
+) => {
   const inputElementRef = useRef<any>(null)
 
   const { registerField, defaultValue = '', fieldName, error } = useField(name)
   const inputValueRef = useRef<InputValueReference>({ value: defaultValue })
+
+  useImperativeHandle(ref, () => ({
+    focus() {
+      inputElementRef.current.focus(inputElementRef)
+    },
+  }))
 
   useEffect(() => {
     registerField<string>({
@@ -52,4 +70,4 @@ const Input: React.FC<InputProps> = ({ name, icon, ...remainder }) => {
   )
 }
 
-export default Input
+export default forwardRef(Input)
