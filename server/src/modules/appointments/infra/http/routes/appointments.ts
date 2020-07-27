c8f746ff7f648576b832d71926321bcfe5635d7a
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-07-27 16:18:13 -0300

    Applying 'Liskov substitution principle' and 'Dependency inversion' on repositories' module

--- server/src/modules/appointments/infra/http/routes/appointments.ts
+++ server/src/modules/appointments/infra/http/routes/appointments.ts
@@ -1,29 +1,29 @@
 import { Router } from 'express'
-import { getCustomRepository } from 'typeorm'
 import { parseISO } from 'date-fns'
 
-import AppointmentsRepository from '@modules/appointments/repositories/AppointmentsRepository'
+import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository'
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService'
 
 import ensureAuthentication from '@modules/users/infra/http/middlewares/ensureAuthentication'
 
 const appointmentsRouter = Router()
+const appointmentsRepository = new AppointmentsRepository()
 
 appointmentsRouter.use(ensureAuthentication)
 
-appointmentsRouter.get('/', async (request, response) => {
-  const appointmentsRepository = getCustomRepository(AppointmentsRepository)
-  const appointments = await appointmentsRepository.find()
+// appointmentsRouter.get('/', async (request, response) => {
+//   const appointmentsRepository = getCustomRepository(AppointmentsRepository)
+//   const appointments = await appointmentsRepository.find()
 
-  return response.json(appointments)
-})
+//   return response.json(appointments)
+// })
 
 appointmentsRouter.post('/', async (request, response) => {
   const { provider_id, date } = request.body
 
   const parsedDate = parseISO(date)
 
-  const CreateAppointment = new CreateAppointmentService()
+  const CreateAppointment = new CreateAppointmentService(appointmentsRepository)
 
   const appointment = await CreateAppointment.execute({
     provider_id,
--- server/src/modules/appointments/services/CreateAppointmentService.ts
+++ server/src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,26 +1,25 @@
 import { startOfHour } from 'date-fns'
-import { getCustomRepository } from 'typeorm'
 
 import AppError from '@shared/errors/AppError'
 
 import Appointment from '../infra/typeorm/entities/Appointment'
-import AppointmentsRepository from '../repositories/AppointmentsRepository'
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository'
 
-interface RequestDTO {
+interface IRequestDTO {
   provider_id: string
   date: Date
 }
 
 class CreateAppointmentService {
+  constructor(private appointmentsRepository: IAppointmentsRepository) {}
+
   public async execute({
     provider_id,
     date
-  }: RequestDTO): Promise<Appointment> {
-    const appointmentsRepository = getCustomRepository(AppointmentsRepository)
-
+  }: IRequestDTO): Promise<Appointment> {
     const appointmentDate = startOfHour(date)
 
-    const findSameDateAppointments = await appointmentsRepository.findByDate(
+    const findSameDateAppointments = await this.appointmentsRepository.findByDate(
       appointmentDate
     )
 
@@ -28,12 +27,10 @@ class CreateAppointmentService {
       throw new AppError('Horário indisponível')
     }
 
-    const appointment = appointmentsRepository.create({
+    const appointment = await this.appointmentsRepository.create({
       provider_id,
       date: appointmentDate
     })
-
-    await appointmentsRepository.save(appointment)
 
     return appointment
   }
