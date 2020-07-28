0129574f8a0049b5da88aa707a49bcc9946bbbf2
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-07-28 17:07:49 -0300

    Tests from 'Appointments' module

---
+++ server/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,42 @@
+import AppError from '@shared/errors/AppError'
+
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository'
+import CreateAppointmentService from './CreateAppointmentService'
+
+describe('CreateAppointent', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository()
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository
+    )
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '791515486'
+    })
+
+    expect(appointment).toHaveProperty('id')
+    expect(appointment.provider_id).toBe('791515486')
+  })
+
+  it('should not be able to create two appointments on the same date and hour', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository()
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository
+    )
+
+    const appointmentDate = new Date(2020, 4, 10, 11)
+
+    await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '791515486'
+    })
+
+    expect(
+      createAppointment.execute({
+        date: appointmentDate,
+        provider_id: '791515486'
+      })
+    ).rejects.toBeInstanceOf(AppError)
+  })
+})
