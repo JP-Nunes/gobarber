b22abf7d5350e87580b85bebd08fd0a944a160d2
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-07-30 15:26:23 -0300

    Implementing password recovery while applying TDD

---
+++ server/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,40 @@
+import { injectable, inject } from 'tsyringe'
+
+import AppError from '@shared/errors/AppError'
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider'
+import IUsersRepository from '../repositories/IUsersRepository'
+import IUserTokensRepository from '../repositories/IUserTokensRepository'
+
+// import User from '../infra/typeorm/entities/User'
+
+interface IRequestDTO {
+  email: string
+}
+
+@injectable()
+class SendForgotPasswordEmailService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
+
+    @inject('userTokensRepository')
+    private userTokensRepository: IUserTokensRepository
+  ) {}
+
+  public async execute({ email }: IRequestDTO): Promise<void> {
+    const existingUser = await this.usersRepository.findByEmail(email)
+
+    if (!existingUser) {
+      throw new AppError('User does not exists.')
+    }
+
+    await this.userTokensRepository.generate(existingUser.id)
+
+    this.mailProvider.sendMail(email, 'Pedido de recuperação de senha recebido')
+  }
+}
+
+export default SendForgotPasswordEmailService
