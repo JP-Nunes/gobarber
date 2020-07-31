f6638dd345b9baf483ad5d63d9e74c8923478b0c
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-07-31 17:09:50 -0300

    Email template

--- server/src/modules/users/services/SendForgotPasswordEmailService.ts
+++ server/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,11 +1,10 @@
 import { injectable, inject } from 'tsyringe'
+import path from 'path'
 
 import AppError from '@shared/errors/AppError'
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider'
 import IUsersRepository from '../repositories/IUsersRepository'
 import IUserTokensRepository from '../repositories/IUserTokensRepository'
-
-// import User from '../infra/typeorm/entities/User'
 
 interface IRequestDTO {
   email: string
@@ -25,18 +24,35 @@ class SendForgotPasswordEmailService {
   ) {}
 
   public async execute({ email }: IRequestDTO): Promise<void> {
-    const existingUser = await this.usersRepository.findByEmail(email)
+    const user = await this.usersRepository.findByEmail(email)
 
-    if (!existingUser) {
+    if (!user) {
       throw new AppError('User does not exists.')
     }
 
-    const { token } = await this.userTokensRepository.generate(existingUser.id)
+    const { token } = await this.userTokensRepository.generate(user.id)
 
-    this.mailProvider.sendMail(
-      email,
-      `Pedido de recuperação de senha recebido ${token}`
+    const forgotPasswordTemplate = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'forgot_password.hbs'
     )
+
+    this.mailProvider.sendMail({
+      to: {
+        name: user.name,
+        email: user.email
+      },
+      subject: '[GoBarber] Recuperação de senha',
+      templateData: {
+        file: forgotPasswordTemplate,
+        variables: {
+          name: user.name,
+          link: `http://localhost:3000/reset_password?${token}`
+        }
+      }
+    })
   }
 }
 
