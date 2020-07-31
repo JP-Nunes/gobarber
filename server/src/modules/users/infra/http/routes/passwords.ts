367b612ebeb323b373c1db96919d909da8d6f304
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-07-30 22:17:12 -0300

    Finishing Password Token implementation and migrations

---
+++ server/src/modules/users/infra/http/routes/passwords.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express'
+
+import ForgotPasswordsController from '../controllers/ForgotPasswordsController'
+import ResetPasswordsController from '../controllers/ResetPasswordsController'
+
+const passwordsRouter = Router()
+const forgotPasswordsController = new ForgotPasswordsController()
+const resetPasswordsController = new ResetPasswordsController()
+
+passwordsRouter.post('/forgot', forgotPasswordsController.create)
+passwordsRouter.post('/', resetPasswordsController.create)
+
+export default passwordsRouter
--- server/src/shared/infra/http/routes/index.ts
+++ server/src/shared/infra/http/routes/index.ts
@@ -3,12 +3,14 @@ import { Router } from 'express'
 import appointmentsRouter from '@modules/appointments/infra/http/routes/appointments'
 import usersRouter from '@modules/users/infra/http/routes/users'
 import sessionsRouter from '@modules/users/infra/http/routes/sessions'
+import passwordsRouter from '@modules/users/infra/http/routes/passwords'
 
 const routes = Router()
 
 routes.use('/appointments', appointmentsRouter)
 routes.use('/users', usersRouter)
 routes.use('/sessions', sessionsRouter)
+routes.use('/passwords', passwordsRouter)
 
 routes.get('/', (request, response) => {
   return response.json({ message: 'Hello moto' })
