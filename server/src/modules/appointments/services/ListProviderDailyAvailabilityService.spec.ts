f59457619ac8855f4f36d210b608c011848eec3b
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-08-03 17:29:14 -0300

    Excluding past hours

--- server/src/modules/appointments/services/ListProviderDailyAvailabilityService.spec.ts
+++ server/src/modules/appointments/services/ListProviderDailyAvailabilityService.spec.ts
@@ -15,12 +15,16 @@ describe('ListProviderMonthAvailability', () => {
   it(`should be able to list provider's day availability`, async () => {
     await fakeAppointementsRepository.create({
       provider_id: 'user',
-      date: new Date(2020, 9, 20, 8, 0, 0)
+      date: new Date(2020, 9, 20, 15, 0, 0)
     })
 
     await fakeAppointementsRepository.create({
       provider_id: 'user',
-      date: new Date(2020, 9, 20, 10, 0, 0)
+      date: new Date(2020, 9, 20, 14, 0, 0)
+    })
+
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 9, 20, 11).getTime()
     })
 
     const availability = await listProviderDailyAvailability.execute({
@@ -33,8 +37,12 @@ describe('ListProviderMonthAvailability', () => {
     expect(availability).toEqual(
       expect.arrayContaining([
         { hour: 8, available: false },
-        { hour: 9, available: true },
-        { hour: 10, available: false }
+        { hour: 9, available: false },
+        { hour: 10, available: false },
+        { hour: 13, available: true },
+        { hour: 14, available: false },
+        { hour: 15, available: false },
+        { hour: 16, available: true }
       ])
     )
   })
