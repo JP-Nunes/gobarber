312c87b7ccf217ee219bf6d95b063ac9272b99fe
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-08-06 16:26:58 -0300

    Creating notifications with appointments

--- server/src/modules/appointments/services/CreateAppointmentService.ts
+++ server/src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,10 +1,11 @@
-import { startOfHour, isBefore, getHours } from 'date-fns'
+import { startOfHour, isBefore, getHours, format } from 'date-fns'
 import { injectable, inject } from 'tsyringe'
 
 import AppError from '@shared/errors/AppError'
 
 import Appointment from '../infra/typeorm/entities/Appointment'
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository'
+import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository'
 
 interface IRequestDTO {
   provider_id: string
@@ -16,7 +17,10 @@ interface IRequestDTO {
 class CreateAppointmentService {
   constructor(
     @inject('AppointmentsRepository')
-    private appointmentsRepository: IAppointmentsRepository
+    private appointmentsRepository: IAppointmentsRepository,
+
+    @inject('NotificationsRepository')
+    private notificationsRepository: INotificationsRepository
   ) {}
 
   public async execute({
@@ -54,6 +58,13 @@ class CreateAppointmentService {
       provider_id,
       user_id,
       date: appointmentDate
+    })
+
+    const formattedDate = format(appointmentDate, 'dd,MM,yyyy às HH:mm')
+
+    await this.notificationsRepository.create({
+      recipient_id: provider_id,
+      content: `Novo agendamento para o dia ${formattedDate}`
     })
 
     return appointment
